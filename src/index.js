be0af1aabe7ca773e55b1f2fc4097080d12ca45b
Tbhate <dmitriybut83@gmail.com>
2024-09-07 20:36:50 +0300

    New section

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-const headerEl = document.getElementById("header") 
+const headerEl = document.getElementById("header")
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     const scrollPos = window.scrollY
 
-    if(scrollPos > 110){
+    if (scrollPos > 110) {
         headerEl.classList.add("header_mini")
-    }else{
+    } else {
         headerEl.classList.remove("header_mini")
     }
 })
 
 
 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const offset = -index * 100;
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(${offset}%)`;
+    });
+}
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}, 5000);
 
 
